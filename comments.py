8abfa50b122e4570c77c35318c31d1289914a937
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-21 15:14:45 +0500

    Write first program in python and learn comments in python

---
+++ comments.py
@@ -0,0 +1,12 @@
+print("I am a good girl hehehhehe")
+# single line comment
+'''
+hjkkl 
+multiline comments
+hjjjjj
+'''
+print("Ayesha")
+print('Ayesha')
+print("I am studying in \"Sindh Madressatul Islam University\"")
+print("Hey", 45, 56, sep='~', end="25\n")
+print("Farah")
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("Hello World", 8)
+print(5)
+print("Bye")
+print(17*14)
