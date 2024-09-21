cda0d429ec2330d24d910347dd43ce484c7f38d4
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-21 19:32:55 +0500

    Learn Comments, Escape Sequences  and more about print statments.

--- Day 01 to 05/comments.py
+++ Day 01 to 05/comments.py
@@ -1,12 +1,12 @@
-print("I am a good girl hehehhehe")
+print("Congragulation Today we Write our second program in python")
 # single line comment
 '''
-hjkkl 
-multiline comments
-hjjjjj
+This is  multiline comments
+for multiline comment we use 
+triple single or double quotes
+
 '''
-print("Ayesha")
-print('Ayesha')
-print("I am studying in \"Sindh Madressatul Islam University\"")
-print("Hey", 45, 56, sep='~', end="25\n")
-print("Abid Hussain")
+print("My name is Ayesha Malik")
+print('My name is Ayesha Malik') # we print our string in single or double quotes 
+print("I am studying in \"Sindh Madressatul Islam University\"") #in double quotes we use backslash to print double quotes
+print("Hey", 45, 56, sep='~', end="25") # sep is used to seprate the values and end is used to print the value at the end of the line 
