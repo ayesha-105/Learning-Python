3269e486975217f0f65303a304a52ab5a68ae90a
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-22 01:27:34 +0500

    Variables and  and Datatypes

---
+++ Day 06 to 10/variables.py
@@ -0,0 +1,17 @@
+a= 45625
+b= 563.85
+c= complex(8, 568)
+d="Ayesha"
+e= True
+f= None
+a1= 9
+print(a)
+print(b) # print statment with variable name 
+print(c)
+print(a + a1) # addition of two numbers 
+print("the type of a is ", type(a))
+print("the type of b is ", type(b))  # type() is used to check the data type of variable
+print("the type of b is ", type(c))
+print("the type of b is ", type(d))
+print("the type of b is ", type(e))
+print("the type of b is ", type(f))
