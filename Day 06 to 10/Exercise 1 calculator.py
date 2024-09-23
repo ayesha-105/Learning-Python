d05cd4a4bb62905d148785898680167fd1b92463
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-23 21:42:03 +0500

    Exercise 1: Create Calculator

---
+++ Day 06 to 10/Exercise 1 calculator.py
@@ -0,0 +1,11 @@
+# create calculator
+a= 3 
+b= 2
+
+print("The value of",a,"+",b, "is:", a+b)
+print("The value of",a,"-",b, "is:", a-b) # doing calculation with operation
+print("The value of",a,"*",b, "is:", a*b)
+print("The value of",a,"/",b, "is:", a/b)
+print("The value of",a,"%",b, "is:", a%b)
+print("The value of",a,"//",b, "is:", a//b)
+print("The value of",a,"**",b, "is:", a**b)
