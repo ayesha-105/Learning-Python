6d63c5b863c88d0445fbb23968d5adff9904fddb
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-27 16:16:39 +0500

    Learn Typecasting / Type conversion and it's two types

--- Day 06 to 10/Typecasting.py
+++ Day 06 to 10/Typecasting.py
@@ -1,1 +1,10 @@
-print("5")
+# Explicit typecasting it one the user 
+a="1"   # string
+b="3"
+print(a + b)   #concatenation
+print(int(a) + int(b))   # we convert string to integer and then add them 
+
+# Implicit typecasting it done by python itself
+c=3
+d= 4.5
+print(c+d)
