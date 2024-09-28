dc68b680035cc5336441fa7d93e7e527a79fe9f3
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-09-28 11:27:44 +0500

    Learn how to take input from user

---
+++ Day 06 to 10/Taking input.py
@@ -0,0 +1,9 @@
+# Take input from user 
+a= input("Enter your name:") # input() is used to take input from user
+print("My name is:",a) 
+
+x= input("Enter your first number:")
+y= input("Enter your second number:")
+print(x+y)               # it will concatenate the two numbers
+
+print(int(x)+ int(y))     # it will add two numbers
